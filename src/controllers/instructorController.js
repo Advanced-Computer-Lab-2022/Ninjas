7dd1281012814bd87934bab0775522ee6595fa61
bassantTarekk <111186666+bassantTarekk@users.noreply.github.com>
2022-10-23 19:12:19 +0200

    Bassant: Instructor Search & filter

--- src/controllers/instructorController.js
+++ src/controllers/instructorController.js
@@ -25,16 +25,84 @@ const instructorController = {
     console.log(result);
     return{result};
    
-    
-        
-        
-    
-        
-        
+    },
 
+    async getViewResult2({
+
+        username
+    }) { 
+      const result2=[]
+      const courses=  await Course.find({
         
+    })
+    for(var i=0;i<courses.length;i++){
+        for(var j=0;j<courses[i].instructors.length;j++){
+            if(courses[i].instructors[j].username==username){
+               result2.push(courses[i]); 
+               break;
+            }
+            
+        }
+
+    }
+    console.log(result2);
+    return{result2};
+   
+    },
 
 
-    }}
+
+    async getSearchResult({
+
+        username, userId , title , subject , instructor , minPrice , maxPrice 
+    }) { 
+
+      const result3 = this.getViewResult2(username)
+
+      const user = await Account.findOne({ _id: userId }, { country: 1 });
+
+
+      const courses = 
+      (subject == null && title == null && instructor == null &&   minPrice == null && maxPrice == null)?
+        await result3.find() :
+        await result3.find({
+        '$or': [
+            { subject: { '$regex': "/^" + subject + "/", '$options': 'i' } },
+            
+            { title: { '$regex': "/^" + title + "/", '$options': 'i' } },
+            
+            {
+                instructors: {
+                    $elemMatch: { firstName: "/^" + instructor + "/" }
+                }
+            },
+            { price: { $gte: minPrice, $lte: maxPrice } }
+        ]
+    });
+
+
+    let details = countryPriceDetails.get(user.country);
+    for (var i = 0; i < courses.length; i++) {
+        // price = price x factor x discount
+        courses[i].price = courses[i].price * details.factor * ((100 - details.discount) / 100);
+    }
+   // console.log(courses);
+    return {courses,currency:details.currency};
+
+      
+
+
+
+
+     
+   
+    console.log(result3);
+    return{result3};
+
+
+
+
+}
+}
 
     module.exports = instructorController;
--- src/routers/instructorRouter.js
+++ src/routers/instructorRouter.js
@@ -15,5 +15,29 @@ instructorRouter.get('/view', async (req, res) => {
     const viewResults = await
         instructorController.getViewResult({ username });
     res.status(200).json({ result: viewResults });
+
+
+
 })
+
+instructorRouter.get('/Searchfilter', async (req, res) => {
+    const { username
+    } = req.body;
+
+    // if (userType != 'Instructor') {
+    //     res.status(401).json({ message: "unauthorized user." });
+    // }
+
+
+
+    const SearchResults = await
+        instructorController.getSearchResult({ username, userId , title , subject , instructor , minPrice , maxPrice });
+    res.status(200).json({ result: SearchResults });
+
+
+
+})
+
+
+
 module.exports = instructorRouter;
