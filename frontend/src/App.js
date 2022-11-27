a077cef0b3fde2b3a64b34834a6b5b6e6435f96f
Power Puff <noraosama88@icloud.com>
2022-11-28 00:37:45 +0200

    ChangePassword

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
+import {BrowserRouter,  Routes, Route} from 'react-router-dom'
+import ChangePassword from './pages/ChangePassword';
+
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element= {<ChangePassword/>} >
+        </Route>
+      </Routes>
 
+      </BrowserRouter>
     </div>
   );
 }
 
-export default App;
+export default App; 
