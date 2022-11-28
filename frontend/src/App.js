ac24a149f699fd6e77c589b994ed50fcdc4af33a
Tarteel <tarteelafattahibrahim@gmail.com>
2022-11-28 22:27:58 +0200

    add routes to frontend app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,12 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import ChangeCountry from './components/ChangeCountry';
 import ForgotPasswordPage from './components/ForgotPassword';
+import InstructorCourses from './components/InstructorCourses';
 import InstructorPage from './components/InstructorHome';
+import RateCourse from './components/RateCourse';
+import SolveExercise from './components/SolveExercise';
 import TraineePage from './components/TraineeHome';
+import TraineeSearch from './components/TraineeSearch';
 import ChangePassword from './pages/ChangePassword';
 
 function App() {
@@ -20,6 +24,13 @@ function App() {
         element={<TraineePage/>}/>
         <Route path="/forgotPassword"
         element={<ForgotPasswordPage/>}/>
+        <Route path="/viewInstructorCourses"
+        element={<InstructorCourses/>}/>
+        <Route path="/traineeSearch"
+        element={<TraineeSearch/>}/>
+        <Route path="/rateCourse" element={<RateCourse/>}/>
+        <Route path="/solveExercise" element={<SolveExercise/>}/>
+
       </Routes>
       </BrowserRouter>
     </div>
