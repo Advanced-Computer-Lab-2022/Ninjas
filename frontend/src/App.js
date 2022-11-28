a2a3f9822bcba07b892458b7b25d50104c259e12
Tarteel <tarteelafattahibrahim@gmail.com>
2022-11-28 06:36:47 +0200

    instructor change country frontned

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import ChangeCountry from './components/ChangeCountry';
+import InstructorPage from './components/InstructorHome';
+
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+      <Routes>
+        <Route path="/instructor"
+        element={<InstructorPage/>}/>
+        <Route path="/changeCountry"
+        element={<ChangeCountry/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
