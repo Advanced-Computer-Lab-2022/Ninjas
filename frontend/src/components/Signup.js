d14b2d892a32c6b68966c8cf5454dc3c03af2bfb
Power Puff <noraosama88@icloud.com>
2022-12-16 20:29:52 +0200

    Signup FE not final

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -11,10 +11,12 @@ import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import Select from '@mui/material/Select';
+import Select,{SelectChangeEvent} from '@mui/material/Select';
 import MenuItem from '@mui/material/Container';
 import FormControl from '@mui/material/Container';
 import InputLabel from '@mui/material/Container';
+import {useState,useEffect} from "react";
+import axios from "axios";
 
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
@@ -34,6 +36,42 @@ function Copyright(props) {
 const theme = createTheme();
 
 export default function Signup() {
+  // username, firstName, lastName, email, password, gender
+  const[firstName,setFirstName]=useState('');
+  const[lastName,setLastName]=useState('');
+  const[email,setEmail]=useState('');
+  const[password,setPassword]=useState('');
+  const[confirm,setConfirm]=useState('');
+  const [gender, setGender] = useState('');
+  const [username, setUsername] = useState('');
+
+  
+
+  const handleChangeGender = (event) => {
+    setGender(event.target.value);
+}
+const handleChangeFirstName = (event) => {
+  setFirstName(event.target.value);
+}
+const handleChangeLastName = (event) => {
+  setLastName(event.target.value);
+}
+const handleChangeEmail = (event) => {
+  setEmail(event.target.value);
+}
+const handleChangePassword = (event) => {
+  setPassword(event.target.value);
+}
+const handleChangeConfirm = (event) => {
+  setConfirm(event.target.value);
+}
+
+const handleChangeUsername = (event) => {
+  setUsername(event.target.value);
+}
+
+
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
@@ -42,6 +80,24 @@ export default function Signup() {
       password: data.get('password'),
     });
   };
+
+  const change = async ()=>{
+    window.location.href=`/`
+    const response=await axios.post(`http://localhost:8000/signUp/`,{username:username,
+     firstName:firstName, 
+     lastName:lastName,
+    email:email,
+    password:password,
+    gender:gender}).
+
+    catch( (error) => alert(error.response.data.message))
+
+
+    console.log(response.data)
+    if(response.status===200){
+        alert(response.data)
+    }}
+    
 
   return (
     <ThemeProvider theme={theme}>
@@ -71,7 +127,10 @@ export default function Signup() {
                   fullWidth
                   id="firstName"
                   label="First Name"
-                  autoFocus
+                  error={firstName === ""}
+                  helperText={firstName === 2 ? 'Empty field!' : ' '}
+                  onChange={(event)=>{handleChangeFirstName(event)}}
+                 autoFocus
                 />
               </Grid>
               <Grid item xs={12} sm={6}>
@@ -82,17 +141,18 @@ export default function Signup() {
                   label="Last Name"
                   name="lastName"
                   autoComplete="family-name"
+                  onChange={(event)=>{handleChangeLastName(event)}}
                 />
               </Grid>
               <Grid item xs={12}>
                 <TextField
                   required
                   fullWidth
-                  name="password"
+                  name="username"
                   label="Username"
-                  type="password"
-                  id="password"
+                  id="username"
                   autoComplete="new-password"
+                  onChange={(event)=>{handleChangeUsername(event)}}
                 />
               </Grid>
               <Grid item xs={12}>
@@ -103,6 +163,7 @@ export default function Signup() {
                   label="Email Address"
                   name="email"
                   autoComplete="email"
+                  onChange={(event)=>{handleChangeEmail(event)}}
                 />
               </Grid>
               <Grid item xs={12}>
@@ -114,6 +175,7 @@ export default function Signup() {
                   type="password"
                   id="password"
                   autoComplete="new-password"
+                  onChange={(event)=>{handleChangePassword(event)}}
                 />
               </Grid>
               <Grid item xs={12}>
@@ -125,49 +187,56 @@ export default function Signup() {
                   type="password"
                   id="password"
                   autoComplete="new-password"
+                  onChange={(event)=>{handleChangeConfirm(event)}}
                 />
               </Grid>
+
+        <div>
+     
+      <FormControl variant="outlined">
+        <InputLabel id="demo-simple-select-filled-label">Gender</InputLabel>
+        <Select
+          fullWidth
+          //labelId="demo-simple-select-filled-label"
+          id="demo-simple-select-filled"
+         // value={gender}
+          onChange={handleChangeGender}
+          autoFocus
+        >
+          <MenuItem value="FEMALE">Female</MenuItem>
+          <MenuItem value="MALE">Male</MenuItem>
+        </Select>
+      </FormControl>
+    </div>
            
-<FormControl sx={{  minWidth: 200}}>
-  <InputLabel id="demo-simple-select-label">Gender</InputLabel>
-  <Select
-    labelId="demo-simple-select-label"
-    id="demo-simple-select"
-    //value={""}
-    label="Age"
-    //onChange={handleChange}
-  >
-    <MenuItem value={"MALE"}>Male</MenuItem>
-    <MenuItem value={"FEMALE"}>Female</MenuItem>
-  </Select>
-</FormControl>
-              <Grid item xs={12}>
-                <FormControlLabel
-                  control={<Checkbox value="allowExtraEmails" color="primary" />}
-                  label="I want to receive inspiration, marketing promotions and updates via email."
-                />
-              </Grid>
-            </Grid>
+      </Grid>
+          
+         
+            
             <Button
               type="submit"
               fullWidth
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
+              onClick={()=> {change()}}
             >
               Sign Up
             </Button>
             <Grid container justifyContent="flex-end">
               <Grid item>
-                <Link href="#" variant="body2">
+                <Link href="/" variant="body2">
                   Already have an account? Sign in
                 </Link>
               </Grid>
             </Grid>
           </Box>
         </Box>
+        
         <Copyright sx={{ mt: 5 }} />
       </Container>
+     
     </ThemeProvider>
+    
   );
 }
 
